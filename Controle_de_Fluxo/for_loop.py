e3b427145494313f07e4c2681f5abe52a4bda394
Artur Fagundes Guimaraes <arturguimaraes.sk8@gmail.com>
2023-08-24 13:41:55 -0300

    feat: added for_loop.py with if and else

---
+++ Controle_de_Fluxo/for_loop.py
@@ -0,0 +1,10 @@
+compra_confirmada = True
+dados_compra = 'Compra no valor de R$12.50 confirmada'
+
+for enviar in range(3):
+    if compra_confirmada:
+        print(dados_compra)
+        print('Detalhes enviados para o seu e-mail.')
+        break
+else:
+    print("Falha na compra")
