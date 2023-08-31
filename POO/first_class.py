b22b7be60b6ffc1081115d0854b93ae65259c242
Artur Guimarães <arturguimaraes.sk8@gmail.com>
2023-08-31 12:29:43 -0300

    feat: added my first class

---
+++ POO/first_class.py
@@ -0,0 +1,10 @@
+class Employees:
+    name = 'Artur'
+    lastName = 'Guimarães'
+    data_nascimento = '08/05/2002'
+
+user1 = Employees()
+
+print(user1.name)
+print(user1.lastName)
+print(user1.data_nascimento)
